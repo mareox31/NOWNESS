ba3966fa469ba8b059a80f8c3e88d826c76bb432
seongguk <func.one@gmail.com>
2023-06-28 15:56:54 +0900

    test: UserController 데이터 전달 테스트
    UserController에서 model의 속성인 data에 hello 를 추가
    login과 signup 페이지를 정상적으로 렌더링하는지 테스트

--- src/main/java/highfive/nowness/controller/UserController.java
+++ src/main/java/highfive/nowness/controller/UserController.java
@@ -1,4 +1,22 @@
 package highfive.nowness.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
 public class UserController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("data", "hello");
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("data", "hello");
+        return "signup";
+    }
 }
