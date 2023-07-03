48541648fe61e5015ad39ed620953802ac274faf
seongguk <func.one@gmail.com>
2023-07-03 11:36:46 +0900

    feat: 회원가입 화면 추가
    회원가입 UI와 로그인 UI가 유사하여 코드 반복을 줄이기 위해서 하나로 통합하고 파일 명칭도 변경하였습니다.
    
    그리고 회원가입 시 입력값 검증을 위한 로직을 추가하고 파일 명칭을 변경하였습니다.

--- src/main/resources/static/js/user/login_signup.js
+++ src/main/resources/static/js/user/login_signup.js
@@ -1,16 +1,28 @@
 $(document).ready(() => {
-    $('#btnLogin').click(() => {
-        const email = $('#floatingEmail');
-        const password = $('#floatingPassword');
-        if (validateLoginForm(email, password)) {
-            loginRequestToServer(email.val(), password.val());
-        }
+    const email = $('#floatingEmail');
+    const password = $('#floatingPassword');
+
+    email.keyup(() => {
+        validateEmail(email);
     });
+    password.keyup(() => {
+        validatePassword(password);
+    });
+
+    if (window.location.pathname === '/user/signup') {
+        const nickname = $('#floatingNickname');
+        const passwordCheck = $('#floatingPasswordCheck');
+
+        nickname.keyup(() => {
+            validateNickname(nickname);
+        });
+
+        passwordCheck.keyup(() => {
+            validatePasswordCheck(password, passwordCheck);
+        });
+    }
+
 });
-
-function loginRequestToServer(email, password) {
-
-}
 
 // login 시 입력되는 email 과 password 를 검증합니다.
 // email 과 password 모두 유효할 경우 true, 그렇지 않을 경우 false 를 반환합니다.
@@ -43,6 +55,31 @@ function validatePassword(password) {
         return true;
     } else {
         password.addClass('is-invalid');
+        return false;
+    }
+}
+
+// password 에 입력한 비밀번호와 확인을 위해 입력한 비밀번호가 일치하는지 검증합니다.
+// 검증 통과 시 true 를 반환하고, 미통과 시 false 를 반환합니다.
+function validatePasswordCheck(password, passwordCheck) {
+    if (password.val() === passwordCheck.val()) {
+        passwordCheck.removeClass('is-invalid');
+        return true;
+    } else {
+        passwordCheck.addClass('is-invalid');
+        return false;
+    }
+}
+
+// 닉네임 규정을 준수하는지 검증합니다.
+// 검증 통과 시 true 를 반환하고, 미통과 시 false 를 반환합니다.
+function validateNickname(nickname) {
+    const regex = /^[가-힣a-zA-Z0-9]{3,8}$/;
+    if (regex.test(nickname.val())) {
+        nickname.removeClass('is-invalid');
+        return true;
+    } else {
+        nickname.addClass('is-invalid');
         return false;
     }
 }
