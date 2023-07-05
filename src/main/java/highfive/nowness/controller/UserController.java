68e3f61ba8ac1369e511e4d9de92f2fbf3b190b0
seongguk <func.one@gmail.com>
2023-07-05 16:03:42 +0900

    test: 로그인 확인 예제 추가
    테스트 겸 로그인 기능의 간단한 사용 예를 추가하였습니다.

--- src/main/java/highfive/nowness/controller/UserController.java
+++ src/main/java/highfive/nowness/controller/UserController.java
@@ -2,11 +2,13 @@ package highfive.nowness.controller;
 
 import highfive.nowness.domain.User;
 import highfive.nowness.service.UserDetailsService;
+import highfive.nowness.util.UserUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,20 +23,16 @@ public class UserController {
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    public UserController(UserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/login")
-    public String showLoginForm(@AuthenticationPrincipal User user) {
-        if (user == null) return "login_signup";
-        else return "redirect:/main";
-    }
-
-    @GetMapping("/signup")
-    public String showSignupForm(@AuthenticationPrincipal User user) {
-        if (user == null) return "login_signup";
+    @GetMapping({"/login", "/signup"})
+    public String showLoginForm(@AuthenticationPrincipal User user,
+                                @AuthenticationPrincipal OAuth2User oAuth2User) {
+        if (UserUtil.isNotLogin(user, oAuth2User)) return "login_signup";
         else return "redirect:/main";
     }
 
@@ -67,8 +65,15 @@ public class UserController {
     }
 
     @GetMapping("/mypage")
-    public String showMypage(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("user", user);
+    public String showMypage(@AuthenticationPrincipal User user,
+                             @AuthenticationPrincipal OAuth2User oAuth2User,
+                             Model model) {
+        if (UserUtil.isNotLogin(user, oAuth2User)) return "redirect:/user/login";
+        if (user == null) user = UserUtil.convertOAuth2UserToUser(oAuth2User);
+        model.addAttribute("id", user.getId());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("nickname", user.getNickname());
         return "mypage";
     }
+
 }
