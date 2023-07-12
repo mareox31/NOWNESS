202e8f195a56084cd2f3010e50295b40e669127e
Developdog <youtaek123@naver.com>
2023-07-13 06:07:09 +0900

    rank 갱신

--- src/main/java/highfive/nowness/controller/RankBoardController.java
+++ src/main/java/highfive/nowness/controller/RankBoardController.java
@@ -1,4 +1,36 @@
 package highfive.nowness.controller;
 
+import highfive.nowness.domain.RankBoard;
+import highfive.nowness.service.RankBoardService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+// 로그 테스트용
+@Slf4j
+@Controller
 public class RankBoardController {
+
+    @Autowired
+    private RankBoardService rankBoardService;
+
+    @RequestMapping(value = "/rankboard", method = RequestMethod.GET)
+    public ModelAndView goHome(HttpServletRequest request) {
+        ModelAndView ran = new ModelAndView();
+        List<RankBoard> RankList = rankBoardService.getRank();
+        ran.addObject("RankList", RankList);
+
+        log.info(RankList.get(0).getContents());
+        log.info(RankList.get(0).getTitle());
+        log.info("잘 작동하는지?");
+        ran.setViewName("rankboard.html");
+        return ran;
+    }
 }
--- src/main/java/highfive/nowness/service/RankBoardService.java
+++ src/main/java/highfive/nowness/service/RankBoardService.java
@@ -1,4 +1,19 @@
 package highfive.nowness.service;
 
+import highfive.nowness.domain.RankBoard;
+import highfive.nowness.repository.RankBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RankBoardService {
+
+    @Autowired
+    private RankBoardRepository rankBoardRepository;
+
+    public List<RankBoard> getRank() {
+        return rankBoardRepository.getRank();
+    }
 }
