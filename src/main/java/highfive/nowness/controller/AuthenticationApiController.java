bcddc26a5b2547b4bee755103fb1ce32578b8729
seongguk <func.one@gmail.com>
2023-07-20 14:33:10 +0900

    fix: mypage script 동작 안하는 문제 해결
    script 불러오는 순서 문제로 순서 조정 및 script 파일 분리

--- src/main/java/highfive/nowness/controller/AuthenticationApiController.java
+++ src/main/java/highfive/nowness/controller/AuthenticationApiController.java
@@ -14,13 +14,18 @@ import java.util.concurrent.atomic.AtomicReference;
 @RequestMapping("/api/v1/user")
 public class AuthenticationApiController {
 
+    /**
+     * 현재 사용자의 로그아웃을 수행합니다. 사용자의 Credentials가 저장된 SecurityContext 를 비우고, 세션을  파기합니다.
+     * @param request the HTTP request object
+     * @return 로그아웃 성공 시 200(success), 실패 시  204(No Content)를 반환
+     */
     @DeleteMapping("/token")
     public ResponseEntity<Boolean> logout(HttpServletRequest request) {
         AtomicReference<ResponseEntity<Boolean>> responseEntity = new AtomicReference<>(ResponseEntity.ok(true));
         Optional.of(SecurityContextHolder.getContext()).ifPresentOrElse(
                 context -> {SecurityContextHolder.clearContext(); request.getSession(false).invalidate();},
                 () -> responseEntity.set(ResponseEntity.noContent().build()));
-
         return responseEntity.get();
     }
+
 }
--- src/main/resources/static/js/user/mypage.js
+++ src/main/resources/static/js/user/mypage.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
     const modal = $('#modal');
     const passwordInput = $('#passwordInput');
     const nicknameInput = $('#nicknameInput');
