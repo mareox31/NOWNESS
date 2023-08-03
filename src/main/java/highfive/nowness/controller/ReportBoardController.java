e9f7d3e4d22971a8d82700587f189acf1d67c8e8
CHAYOUBIN <youbin5412@naver.com>
2023-08-03 11:15:29 +0900

    feat: 제보게시판 오류수정

--- src/main/java/highfive/nowness/controller/ReportBoardController.java
+++ src/main/java/highfive/nowness/controller/ReportBoardController.java
@@ -2,6 +2,7 @@ package highfive.nowness.controller;
 
 import highfive.nowness.dto.PostData;
 import highfive.nowness.dto.ReportDTO;
+import highfive.nowness.dto.TagsDTO;
 import highfive.nowness.service.ReportBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -49,13 +50,21 @@ public class ReportBoardController {
     @GetMapping("/board")
     public String reportBoardList(@RequestParam(defaultValue = "1") int page, Model model) {
         int pageSize = 10;
-        int totalPosts = reportBoardService.getTotalPostsCount();
+        int totalPosts;
+        List<ReportDTO> posts;
+
+
+        // boardType이 2인 게시글 수만 가져오도록 변경
+        totalPosts = reportBoardService.getTotalPostsCountByBoardType(2);
+
         int totalPages = (int) Math.ceil((double) totalPosts / pageSize);
 
         page = Math.max(1, Math.min(page, totalPages));
 
         int offset = (page - 1) * pageSize;
-        List<ReportDTO> posts = reportBoardService.getPostsByPage(offset, pageSize);
+
+        // boardType이 2인 게시글만 가져오도록 변경
+        posts = reportBoardService.getPostsByBoardTypeAndPage(2, offset, pageSize);
 
         model.addAttribute("posts", posts);
         model.addAttribute("currentPage", page);
@@ -75,37 +84,83 @@ public class ReportBoardController {
         // 게시물의 조회수를 증가시키고 DB에 조회수 증가
         reportBoardService.increasePostViewCount(postId);
 
+        // 게시글의 태그 정보를 가져옵니다.
+        List<TagsDTO> tags = reportBoardService.getTagsByContentsId(postId);
+
         model.addAttribute("post", post);
+        model.addAttribute("tags", tags);
         return "reportpost";
     }
 
     // 게시글 목록 조회 및 검색(제목으로 구현)
+    // 게시글 검색후 페이징처리 전체게시글로 가는 오류 수정
     @GetMapping("/board/search")
-    public String reportBoardList(@RequestParam(defaultValue = "1") int page,
-                                  @RequestParam(required = false) String searchTitle,
-                                  Model model) {
+    public String reportBoardListBySearch(@RequestParam(defaultValue = "1") int page,
+                                          @RequestParam(required = false) String searchTitle,
+                                          Model model) {
         int pageSize = 10;
         int totalPosts;
         List<ReportDTO> posts;
 
         if (searchTitle != null && !searchTitle.isEmpty()) {
             // 검색어가 존재하는 경우 검색 결과만 가져옴
-            totalPosts = reportBoardService.getTotalPostsCountByTitle(searchTitle);
-            posts = reportBoardService.getPostsByTitleAndPage(searchTitle, page, pageSize);
+            totalPosts = reportBoardService.getTotalPostsCountByTitleAndBoardType(searchTitle, 2);
+            posts = reportBoardService.getPostsByTitleAndBoardTypeAndPage(searchTitle, 2, page, pageSize);
         } else {
-            // 검색어가 없는 경우 게시글 목록 1페이지를 가져옴
-            totalPosts = reportBoardService.getTotalPostsCount();
+            // 검색어가 없는 경우 boardType이 2인 게시글 목록 1페이지를 가져옴
+            totalPosts = reportBoardService.getTotalPostsCountByBoardType(2);
             int totalPages = (int) Math.ceil((double) totalPosts / pageSize);
             page = Math.max(1, Math.min(page, totalPages));
             int offset = (page - 1) * pageSize;
-            posts = reportBoardService.getPostsByPage(offset, pageSize);
+            posts = reportBoardService.getPostsByBoardTypeAndPage(2, offset, pageSize);
         }
 
         model.addAttribute("posts", posts);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", (int) Math.ceil((double) totalPosts / pageSize));
 
+        // 검색어를 URL 파라미터로 전달하여 페이지 이동에 유지되도록 함
+        model.addAttribute("searchTitle", searchTitle);
+
         return "/reportboard";
+    }
+
+    // 게시글 수정페이지로 이동
+    @GetMapping("/board/{postId}/edit")
+    public String editPostForm(@PathVariable int postId, Model model) {
+        ReportDTO post = reportBoardService.getPostById(postId);
+        if (post == null) {
+            return "redirect:/report/board";
+        }
+
+        model.addAttribute("post", post);
+        return "reportedit";
+    }
+
+    // 게시글 삭제
+    @GetMapping("/board/{postId}/delete")
+    public String deletePost(@PathVariable int postId, Model model) {
+        // 삭제하기 전에 게시글 정보를 확인하기 위해 게시글 정보를 가져옵니다.
+        ReportDTO post = reportBoardService.getPostById(postId);
+        if (post == null) {
+            // 게시글이 존재하지 않을 경우 게시판으로 리다이렉트합니다.
+            return "redirect:/report/board";
+        }
+
+        // 삭제 확인 페이지에 게시글 정보를 전달합니다.
+        model.addAttribute("post", post);
+
+        return "reportdeleteconfirmation"; // 삭제 확인 페이지에 해당하는 Thymeleaf 템플릿을 작성합니다.
+    }
+
+    // 게시글 삭제 처리를 위한 핸들러 메서드 추가
+    @PostMapping("/board/{postId}/delete")
+    public String processDeletePost(@PathVariable int postId) {
+        // postId를 이용하여 게시글을 삭제합니다.
+        reportBoardService.deletePostById(postId);
+
+        // 삭제가 성공하면 게시판으로 리다이렉트합니다.
+        return "redirect:/report/board";
     }
 
 }
--- src/main/java/highfive/nowness/repository/ReportBoardRepository.java
+++ src/main/java/highfive/nowness/repository/ReportBoardRepository.java
@@ -2,6 +2,7 @@ package highfive.nowness.repository;
 
 import highfive.nowness.dto.PostData;
 import highfive.nowness.dto.ReportDTO;
+import highfive.nowness.dto.TagsDTO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
@@ -49,6 +50,37 @@ public class ReportBoardRepository {
     public List<ReportDTO> getPostsByTitleAndPage(String searchTitle, int offset, int pageSize) {
         Map<String, Object> params = Map.of("searchTitle", searchTitle, "offset", offset, "pageSize", pageSize);
         return sql.selectList("report.getPostsByTitleAndPage", params);
+    }
+
+    // 제보요청카테고리만 게시물 목록 출력
+    public int getTotalPostsCountByBoardType(int boardType) {
+        return sql.selectOne("report.getTotalPostsCountByBoardType", boardType);
+    }
+
+    public List<ReportDTO> getPostsByBoardTypeAndPage(int boardType, int offset, int pageSize) {
+        Map<String, Integer> params = Map.of("boardType", boardType, "offset", offset, "pageSize", pageSize);
+        return sql.selectList("report.getPostsByBoardTypeAndPage", params);
+    }
+
+    // 제보요청카테고리만 검색 목록 출력
+    public int getTotalPostsCountByTitleAndBoardType(String searchTitle, int boardType) {
+        Map<String, Object> params = Map.of("searchTitle", searchTitle, "boardType", boardType);
+        return sql.selectOne("report.getTotalPostsCountByTitleAndBoardType", params);
+    }
+
+    public List<ReportDTO> getPostsByTitleAndBoardTypeAndPage(String searchTitle, int boardType, int offset, int pageSize) {
+        Map<String, Object> params = Map.of("searchTitle", searchTitle, "boardType", boardType, "offset", offset, "pageSize", pageSize);
+        return sql.selectList("report.getPostsByTitleAndBoardTypeAndPage", params);
+    }
+
+    // 게시글 삭제
+    public void deletePostById(int postId) {
+        sql.delete("report.deletePostById", postId);
+    }
+
+    // 글 상세 정보 조회 시 해당 글의 태그 정보도 함께 조회
+    public List<TagsDTO> getTagsByContentsId(int contentsId) {
+        return sql.selectList("report.getTagsByContentsId", contentsId);
     }
 
 }
